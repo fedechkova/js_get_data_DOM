786a313baf2534d8eb158e41fb33f709c86f6140
fedechkova <fedechko.anya@gmail.com>
2026-03-13 19:43:25 +0100

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const populationElement = document.querySelectorAll('.population');
+
+const populations = Array.from(populationElement).map((element) => {
+  const text = element.textContent.replace(/,/g, '').trim();
+
+  return Number(text);
+});
+
+const totalPopulation = populations.reduce((sum, value) => sum + value, 0);
+const averagePopulation = totalPopulation / populations.length;
+
+document.querySelector('.average-population').textContent =
+  Math.round(averagePopulation).toLocaleString();
+
+document.querySelector('.total-population').textContent =
+  totalPopulation.toLocaleString();
